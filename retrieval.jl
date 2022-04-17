9f35ef871675e643077be54966cbce0473a9160f
Daniel Civita Ramirez <69374365+daniel-civitaramirez@users.noreply.github.com>
2022-04-17 19:02:45 -0400

    stencil code bug

---
+++ retrieval.jl
@@ -0,0 +1,34 @@
+using HTTP
+using JSON
+
+
+function create_TA2C_from_file(filename::String = ".keys")::Dict
+    keys = Dict()
+  
+    isfile(filename) || exit("File not found: " + filename)
+  
+    open(filename) do f
+        for line in eachline(f)
+            line = strip(line)
+            array = split(line, "=")
+            keys[array[1]] = strip(array[2])
+        end
+    end
+
+    return keys
+end
+
+TA2c = create_TA2C_from_file()
+
+function make_GET_req(url, body)
+    header = ["Authorization"=>"Bearer "* TA2c["token"], "User-Agent"=>"Twitter-API-sample-code"]
+    return HTTP.request("GET", url, header, HTTP.Form(body))
+end
+
+query_params = Dict(
+    "query"=>"zoloft",
+    "tweet.fields"=>"author_id"
+)
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+http_r = make_GET_req(search_url, query_params)
