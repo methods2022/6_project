2b7c3c1edaf8aa2a801005b2bbb09c8d6c71b5b2
darber-48 <60905236+darber-48@users.noreply.github.com>
2022-05-01 17:55:12 -0400

    Update sentiment_analysis_nltk.py

--- sentiment_analysis_nltk.py
+++ sentiment_analysis_nltk.py
@@ -8,6 +8,7 @@ import os
 import nltk
 import re
 import string
+import pandas as pd
 
 #nltk.downloader.download('vader_lexicon')
 
@@ -32,30 +33,35 @@ neu_count = 0
 # we also have a polarity sore that can go from -1 to 1 and 1 indicating very positive and -1 very negative
 total_polarity = 0
 
+# Here is the fetching of the raw data
+file_path="/gpfs/data/biol1555/projects2022/team06/6_project/data/celexa.txt"
+processed_data = open(file_path,'r')#,encoding="utf-8")
 
-example_tweet = "this drug is the worst thing ever!!! @biden"
+tweets = [line.decode('utf-8').strip() for line in processed_data.readlines()]
 
-analysis = TextBlob(example_tweet)
-#use the sentiment analyzer to get the polarity scores, dictionary stores it all
-score = SentimentIntensityAnalyzer().polarity_scores(example_tweet)
+for tweet in tweets:
+    
+    analysis = TextBlob(tweet)
 
-# update the total polarity score by accessing the individual polarity of that single tweet
-total_polarity += analysis.sentiment.polarity
+    #use the sentiment analyzer to get the polarity scores, dictionary stores it all
+    score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+    
+    # update the total polarity score by accessing the individual polarity of that single tweet
+    total_polarity += analysis.sentiment.polarity
 
-# if the negative score is greater than positive score it is a negative tweet
-if score['neg'] > score['pos']:
-    neg_count+=1
-    neg_list.append(example_tweet)
+    # if the negative score is greater than positive score it is a negative tweet
+    if score['neg'] > score['pos']:
+        neg_count+=1
+        neg_list.append(tweet)
 
-# otherwise positive could be greater than the negative
-elif scor['pos']>score['neg']:
-    pos_count+=1
-    pos_list.append(example_tweet)
+    # otherwise positive could be greater than the negative
+    elif score['pos']>score['neg']:
+        pos_count+=1
+        pos_list.append(tweet)
 
-# here we must be a neutral tweet
-else:
-    neu_count+=1
-    neu_list.append(example_tweet)
-
+    # here we must be a neutral tweet
+    else:
+        neu_count+=1
+        neu_list.append(tweet)
 
 print("this is the count for each three counts: ",neg_count,pos_count,neu_count)
