09af8d7b7d6fb175c09b36a6d01e43bb6bb2b553
darber-48 <60905236+darber-48@users.noreply.github.com>
2022-05-02 18:02:16 -0400

    Update sentiment_analysis_nltk.py

--- sentiment_analysis_nltk.py
+++ sentiment_analysis_nltk.py
@@ -9,6 +9,7 @@ import nltk
 import re
 import string
 import pandas as pd
+import os
 
 #nltk.downloader.download('vader_lexicon')
 
@@ -20,48 +21,70 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-# we create a few lists that will contains the tweets relevant to a pos, neg or neu descriptor
-pos_list = []
-neg_list = []
-neu_list = []
-
-# now we also have three counters to keep track of the number of positive, negative or neutral tweets
-pos_count = 0
-neg_count = 0
-neu_count = 0
-
-# we also have a polarity sore that can go from -1 to 1 and 1 indicating very positive and -1 very negative
-total_polarity = 0
-
 # Here is the fetching of the raw data
-file_path="/gpfs/data/biol1555/projects2022/team06/6_project/data/celexa.txt"
-processed_data = open(file_path,'r')#,encoding="utf-8")
+#file_path="/gpfs/data/biol1555/projects2022/team06/6_project/data/celexa.txt"
+#processed_data = open(file_path,'r')#,encoding="utf-8")
 
-tweets = [line.decode('utf-8').strip() for line in processed_data.readlines()]
 
-for tweet in tweets:
+
+def get_data_filepaths(drug):
+    file = drug+".txt"
+    return os.path.join('/gpfs/data/biol1555/projects2022/team06/6_project/data/', file)
+
+def get_raw_tweets(filepath):
+    raw_tweets = []
+    datafile = open(filepath, 'r')
+    for line in datafile.readlines():
+        raw_tweet = line.split('|')[1]
+        if raw_tweet == "raw_tweet":
+            continue
+        raw_tweets.append(raw_tweet)
+    return raw_tweets
+
+def sentiment_analysis(documents):
+    tweets = [line.decode('utf-8').strip() for line in documents]
+
+    # now we also have three counters to keep track of the number of positive, negative or neutral tweets
+    pos_count = 0
+    neg_count = 0
+    neu_count = 0
+
+    # we also have a polarity sore that can go from -1 to 1 and 1 indicating very positive and -1 very negative
+    total_polarity = 0
+
+    for tweet in tweets:
+        analysis = TextBlob(tweet)
+
+        #use the sentiment analyzer to get the polarity scores, dictionary stores it all
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet)
     
-    analysis = TextBlob(tweet)
+        # update the total polarity score by accessing the individual polarity of that single tweet
+        total_polarity += analysis.sentiment.polarity
 
-    #use the sentiment analyzer to get the polarity scores, dictionary stores it all
-    score = SentimentIntensityAnalyzer().polarity_scores(tweet)
-    
-    # update the total polarity score by accessing the individual polarity of that single tweet
-    total_polarity += analysis.sentiment.polarity
+        # if the negative score is greater than positive score it is a negative tweet
+        if score['neg'] > score['pos']:
+            neg_count+=1
 
-    # if the negative score is greater than positive score it is a negative tweet
-    if score['neg'] > score['pos']:
-        neg_count+=1
-        neg_list.append(tweet)
+        # otherwise positive could be greater than the negative
+        elif score['pos']>score['neg']:
+            pos_count+=1
 
-    # otherwise positive could be greater than the negative
-    elif score['pos']>score['neg']:
-        pos_count+=1
-        pos_list.append(tweet)
+        # here we must be a neutral tweet
+        else:
+            neu_count+=1
 
-    # here we must be a neutral tweet
-    else:
-        neu_count+=1
-        neu_list.append(tweet)
 
-print("this is the count for each three counts: ",neg_count,pos_count,neu_count)
+def main():
+    drug_list = ["zoloft", "cymbalta", "celexa", "viibryd", "pristiq"]
+    for drug in drug_list:
+
+        filepath = get_data_filepaths(drug)
+        raw_tweets = get_raw_tweets(filepath)
+        sentiment_analysis(raw_tweets)
+
+    #print(f'{drug} sentiment results: {sentiments}')
+    print('all done!')
+
+if __name__ == "__main__":
+    main()
+#print("this is the count for each three counts: ",neg_count,pos_count,neu_count)
