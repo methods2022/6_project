30651d77ab82aa43fd662a9262899b5f2b856977
Harshini <57264480+harshini225@users.noreply.github.com>
2022-05-06 09:36:30 -0400

    updated vader julia

--- vader-twitter.jl
+++ vader-twitter.jl
@@ -2,12 +2,47 @@ using VaderSentiment
 analyzer = VaderSentiment.SentimentIntensityAnalyzer
 
 
-# input file 
-# remove first row of the input file 
+function process_input(input_filename)
+    input_file = open(input_filename, "r")
+    line_count = 0
+    all_tweets = []
 
-function main() 
-    str_list = String["zoloft", "cymblata", "pristiq", "celexa", "viibryd"]
-    drug_dict = Dict()
+    for line in eachline(input_file)
+        # skip first line - header 
+        line_count += 1
+        if line_count == 1
+            continue
+        end
+        
+        # split line
+        line_part_array = split(line,"|")
+
+        # retrieve processed tweet
+        push!(all_tweets, line_part_array[3])
+    end 
+    close(input_file)
+    return all_tweets
+end 
+
+
+function iterate_over_all_drugs(output_filename)
+    data_folder = "data/"
+    data_text_paths = ["zoloft.txt", "cymbalta.txt", "pristiq.txt", "celexa.txt", "viibryd.txt"]
+
+    output_file = open(output_filename, "w")
+    for data_path in data_text_paths
+        #input_path = data_folder*data_path
+        input_path = "twitter-example.txt"
+        tweets = process_input(input_path)
+        score = analyze(tweets)
+        drug_name = split(data_path, ".")[1]
+        write(output_file, "$drug_name score: $score\n")
+    end
+    close(output_file)
+end 
+
+# analyses the strings (tweets)
+function analyze(str_list) 
     size = length(str_list)
     sum_neg = 0.0
     sum_neu = 0.0
@@ -28,6 +63,10 @@ function main()
     average_pos = sum_pos / size
 
     average_compound = sum_compound / size
+    return average_compound
 end
 
-main()
+
+# puts everything together
+iterate_over_all_drugs("vader_julia_output.txt")
+# analyze(process_input("twitter-example.txt")) - for testing 
