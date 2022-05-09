9e537161ecf3338f1ed5e48fa58651d8844744c5
Juan Pablo Ramos Barroso <78126657+jpramosbarroso@users.noreply.github.com>
2022-05-09 14:28:16 -0400

    Update visualization_nltk.jl

--- visualization_nltk.jl
+++ visualization_nltk.jl
@@ -1,2 +1,24 @@
+using PyPlot
+
 VISUALIZATION_DIR = "visualization/"
 VISUALIZATION_ANALYSIS_FILE = "visualization/nltk_analysis_output.txt"
+
+input = open("visualization/nltk_analysis_output.txt", "r")
+
+xlab = ["positive", "negative", "neutral"]
+
+for line in readlines(input)
+    
+    array = split(line, "|")
+
+    data = [parse(Int, array[2]), parse(Int, array[3]), parse(Int, array[4])]
+
+    drug = array[1]
+
+    b = bar(xlab,data, align="center", color= ["blue","orange","green"],alpha=0.4)
+    PyPlot.title("$drug Tweet Sentiment Analysis by NLTK")
+    output_file = "$(drug)_nltk_python.png"
+
+    plt.savefig(output_file)
+
+end
